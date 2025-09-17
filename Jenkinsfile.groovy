5d410d93c5d14adfabfb1dfeb2d888b1eb7068e6
svelaga2704 <svelaga2704@gmail.com>
2025-09-17 15:04:46 -0500

    Added Jenkinsfile and Kaniko config

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,40 +1,31 @@
 pipeline {
     agent any
-
     environment {
-        DOCKERHUB_CREDENTIALS = credentials('dockerhub')
         IMAGE_NAME = "svelaga2704/ci-cd-demo"
+        IMAGE_TAG = "v1"
     }
-
     stages {
         stage('Checkout') {
             steps {
-                git branch: 'main', url: 'https://github.com/svelaga2704/CI-CD-Pipeline-jenkins_Kubernetes_Helm_Automation.git'
+                git branch: 'main',
+                    url: 'https://github.com/svelaga2704/CI-CD-Pipeline-jenkins_Kubernetes_Helm_Automation.git'
             }
         }
-
-        stage('Build Docker Image') {
+        stage('Build & Push with Kaniko') {
             steps {
-                script {
-                    sh 'docker build -t $IMAGE_NAME:$BUILD_NUMBER .'
+                container('kaniko') {
+                    sh '''
+                    /kaniko/executor \
+                        --dockerfile=Dockerfile.dockerfile \
+                        --context=`pwd` \
+                        --destination=$IMAGE_NAME:$IMAGE_TAG
+                    '''
                 }
             }
         }
-
-        stage('Push to Docker Hub') {
-            steps {
-                script {
-                    sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
-                    sh 'docker push $IMAGE_NAME:$BUILD_NUMBER'
-                }
-            }
-        }
-
         stage('Deploy to Kubernetes') {
             steps {
-                script {
-                    sh 'kubectl set image deployment/myapp myapp=$IMAGE_NAME:$BUILD_NUMBER -n dev || kubectl apply -f k8s/deployment.yaml -n dev'
-                }
+                sh 'kubectl apply -f k8s/deployment.yaml -n ci'
             }
         }
     }
